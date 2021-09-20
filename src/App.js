3ce4297d79fe12d6fc0ea77e9da78933361b74e2
BelalElolahi <belalelolahi003@gmail.com>
2021-09-19 20:44:05 -0700

    git data from bakend and display it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*import Footer from './components/Footer';
+import Header from './components/Header';*/
+import BestBooks from './components/BestBooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ class App extends Component {
+     constructor(props){
+         super(props);
+         this.state ={
+            data:[]
+         };
+     }
+
+
+   componentDidMount() {
+    axios.get(`http://localhost:8000/get-data`)
+      .then(res => {
+       //  console.log(res.data);
+        this.setState({
+            data: res.data,
+            showBooks:true,
+            showAlert:false     
+        });
+       // console.log(this.state.data);
+
+      }).catch(err =>{
+       console.log(err);
+        this.setState({
+            showBooks:false,
+            showAlert:true     
+        });
+
+      });
+  }
+    render() {
+        return (
+            <div className="row">
+                {
+                    this.state.data.map(itme =>{
+                        return <BestBooks  title={itme.title} descriptions={itme.descriptions} email={itme.email}/>
+                    })
+                }
+
+                   
+                
+                   
+            </div>
+        );
+    }
 }
 
 export default App;
