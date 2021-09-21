78e3771e4748dd26a891312fd31582b7b8c8e84d
BelalElolahi <belalelolahi003@gmail.com>
2021-09-20 17:23:30 -0700

    Connect with the server to Delete nad create data

---
+++ src/components/bookForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+export class bookForm extends Component {
+    render() {
+        return (
+            <div>
+                
+
+
+
+            </div>
+        )
+    }
+}
+
+export default bookForm
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Home Page 
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default index
